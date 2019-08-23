0b618b7de8d4010ac754a86069c1eabe1033569e
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2019-08-23 16:50:02 +0100

    Added script to build docker images and apply kubernetes deployment <Dani>

---
+++ run-minikube.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+printf "Building docker images..\n"
+docker build -t alfred-api:latest alfred-api/
+docker build -t alfred-frontend:latest alfred-frontend/
+
+
+printf "Apply kubernetes deployment in minikube\n"
+kubectl.exe apply -f kubernetes/local/
